9f88c85ad47b0ab8a8a3f2742f77909e0a38549c
Saurabh <109336554+SaurabhDayal@users.noreply.github.com>
2025-01-13 19:34:57 +0530

    s

--- src/creational/prototypePattern/prototypePkg/VehicleCache.java
+++ src/creational/prototypePattern/prototypePkg/VehicleCache.java
@@ -6,16 +6,17 @@ import java.util.Map;
 
 public class VehicleCache {
 
-    private final Map<String, Vehicle> cache = new HashMap<>();
+    private final Map<String, Vehicle> cache;
 
     public VehicleCache() {
-        Car car = new Car("Bugatti", "Chiron", "Blue", 261);
-        Bus bus = new Bus("Mercedes", "Setra", "White", 48);
 
-        cache.put("Bugatti Chiron", car);
-        cache.put("Mercedes Setra", bus);
+        cache = new HashMap<>();
+        // Prepopulate the cache with default vehicles
+        cache.put("Bugatti Chiron", new Car("Bugatti", "Chiron", "Blue", 261));
+        cache.put("Mercedes Setra", new Bus("Mercedes", "Setra", "White", 48));
     }
 
+    // Get retrieves a clone of the vehicle corresponding to the given key.
     public Vehicle get(String key) {
         Vehicle vehicle = cache.get(key);
         if (vehicle == null) {
@@ -24,6 +25,7 @@ public class VehicleCache {
         return vehicle.clone();
     }
 
+    // Put adds a list of vehicles to the cache.
     public void put(List<Vehicle> vehicles) {
         vehicles.forEach(vehicle -> cache.put(vehicle.getBrand() + " " + vehicle.getModel(), vehicle));
     }
